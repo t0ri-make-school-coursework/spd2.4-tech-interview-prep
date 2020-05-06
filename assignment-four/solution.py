cd30998cd0bb5e9b54ceb823a3f6e134187007d9
t0ri <victoriarosemurray@gmail.com>
2020-05-06 11:34:52 -0700

    add solution

---
+++ assignment-four/solution.py
@@ -0,0 +1,41 @@
+class LinkedListNode: 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+  
+class LinkedList: 
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, new_data): 
+        node = LinkedListNode(new_data)
+        node.next = self.head
+        self.head = node
+
+  
+    def rotate_by_k(self, k):
+        node = self.head 
+        count = 1 
+        
+        while count < k and node is not None:
+            node = node.next
+            count += 1
+
+        last = node  
+      
+        while node.next is not None: 
+            node = node.next
+  
+        node.next = self.head
+        self.head = last.next
+        last.next = None
+  
+  
+llist = LinkedList() 
+llist.prepend(1)
+llist.prepend(2)
+llist.prepend(3)
+llist.prepend(4)
+llist.prepend(5)
+llist.prepend(6)
+llist.rotate_by_k(4)
